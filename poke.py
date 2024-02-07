9b1f37e461a07738f391501eaa3d19381581282c
Aymen Shoteri <aymenshoteri@gmail.com>
2024-02-07 00:00:24 -0500

    Completed Logic, need list editing

---
+++ poke.py
@@ -0,0 +1,24 @@
+import requests
+import pprint as pp
+
+url = "https://pokeapi.co/api/v2/pokemon/"
+params = {'limit': 100}
+
+pokeList = []
+for offset in range(0, 1025, 100):
+    params['offset'] = offset  # add new value to dict with `limit`
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200: 
+        print(response.text)
+    else:
+        data = response.json()
+        #pp.pprint(data)
+        for item in data['results']:
+            id = item['url'].split('/')[-2]
+            pokeList.append([id, item['name']])
+            if id == '550':
+                print(pokeList)
+                pokeList = []
+
+print(pokeList)
