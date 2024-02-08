ed58cd126292172a7f9567914f6822bff2a879e4
Aymen Shoteri <aymenshoteri@gmail.com>
2024-02-07 19:48:33 -0500

    fixed naming logic, need to fix shadow logic

--- poke.py
+++ poke.py
@@ -5,7 +5,7 @@ url = "https://pokeapi.co/api/v2/pokemon/"
 params = {'limit': 100}
 
 pokeList = []
-for offset in range(0, 1025, 100):
+for offset in range(0, 1500, 100):
     params['offset'] = offset  # add new value to dict with `limit`
     response = requests.get(url, params=params)
 
@@ -17,8 +17,9 @@ for offset in range(0, 1025, 100):
         for item in data['results']:
             id = item['url'].split('/')[-2]
             pokeList.append([id, item['name']])
-            if id == '550':
-                print(pokeList)
+            if id == '800':
                 pokeList = []
 
 print(pokeList)
+
+
